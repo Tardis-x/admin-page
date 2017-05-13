668239c952b1c0310b082b02e6496532f6f26d52
Oleksandr Pidlisnyi <oleksandr.pidlisnyi@gmail.com>
2017-05-14 02:40:34 +0300

    changes

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,71 +1,3 @@
-// // const organizationsActions = {
-// //   createOrganization: (data) => {
-// //     firebase.database()
-// //       .ref(`/organizations`)
-// //       .push(data)
-// //       .then(d => {
-// //         console.log('created', d);
-// //       })
-// //       .catch(error => {
-// //         console.log(error);
-// //       });
-// // //      speakerActions.fetchSpeaker(key);
-// //   },
-// //   fetchOrganizations: () => {
-// //     firebase.database().ref('/organizations').once('value', snapshot => {
-// //       store.dispatch({
-// //         type: 'FETCH_ORGANIZATIONS_SUCCESS',
-// //         data: snapshot.val()
-// //       });
-// //     });
-// //   },
-// //   fetchOrganization: (key) => {
-// //     firebase.database().ref(`/organizations/${key}`).once('value', snapshot => {
-// //       store.dispatch({
-// //         type: 'FETCH_ORGANIZATION_SUCCESS',
-// //         data: snapshot.val()
-// //       });
-// //     });
-// //   },
-// //   updateOrganization: (key, data) => {
-// //     firebase.database().ref(`/organizations/${key}`).set(data);
-// //     speakerActions.fetchSpeaker(key);
-// //   },
-// // };
-//
-// const createOrganization = (data) => {
-//
-// };
-//
-// const createOrganizationFailure = (err) => {
-//
-// };
-//
-// const createOrganizationSuccess = (data) => {
-//
-// };
-//
-// const fetchOrganizations = () => {
-//
-// };
-//
-// const fetchOrganizationsFailure = () => {
-//
-// };
-//
-// const fetchOrganizationsSuccess = () => {
-//
-// };
-//
-// export {
-//   createOrganization,
-//   createOrganizationFailure,
-//   createOrganizationSuccess,
-//   fetchOrganizations,
-//   fetchOrganizationsFailure,
-//   fetchOrganizationsSuccess,
-// }
-
 const createOrganizationSuccess = (d) => ({
   type: CREATE_ORGANIZATION_SUCCESS,
   data: d,
@@ -123,7 +55,6 @@ const organizationsActions = {
       .put(file, metadata)
       .then(snapshot => {
         const url = snapshot.downloadURL;
-
         store.dispatch(organizationsActions.uploadOrganizationLogoSuccess({ url }));
       })
       .catch(error => {
