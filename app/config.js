f11627e136bb4720489c6e73ea8389e186a961f3
Oleksandr Pidlisnyi <oleksandr.pidlisnyi@gmail.com>
2017-09-04 12:04:11 +0300

    WIP

---
+++ app/config.js
@@ -0,0 +1,10 @@
+const config = {
+  firebase: {
+    apiKey: 'AIzaSyD8iuE0X2JIPbKVHqgTTli1eM3Z7Ovj61A',
+    authDomain: 'tardis-tst-hoverboard.firebaseapp.com',
+    databaseURL: 'https://tardis-tst-hoverboard.firebaseio.com',
+    projectId: 'tardis-tst-hoverboard',
+    storageBucket: 'tardis-tst-hoverboard.appspot.com',
+    messagingSenderId: '1089999606283'
+  },
+};
